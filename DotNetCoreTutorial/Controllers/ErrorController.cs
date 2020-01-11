5c76900213ff817bd49ad041cd51a8de95541d39
GeorgeMitsikaris <george.mitsikaris@gmail.com>
2020-01-11 12:48:20 +0200

    Implemented global exception handling

--- DotNetCoreTutorial/Controllers/ErrorController.cs
+++ DotNetCoreTutorial/Controllers/ErrorController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCoreTutorial.Controllers
@@ -18,6 +20,19 @@ namespace DotNetCoreTutorial.Controllers
                     break;
             }
             return View("NotFound");
+        }
+
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+
+            return View();
         }
     }
 }
--- DotNetCoreTutorial/Controllers/HomeController.cs
+++ DotNetCoreTutorial/Controllers/HomeController.cs
@@ -29,6 +29,7 @@ namespace DotNetCoreTutorial.Controllers
 
         public ViewResult Details(int? id)
         {
+            throw new Exception();
             Employee employee = employeeRepository.GetEmployee(id.Value);
 
             if (employee == null)
