38169f70198db0f1e3aab7104dceea058f52fc89
GeorgeMitsikaris <george.mitsikaris@gmail.com>
2020-01-19 13:55:08 +0200

    Created Register user view

---
+++ DotNetCoreTutorial/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTutorial.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match ")]
+        public string ConfirmPassword { get; set; }
+    }
+}
