b2d42b6c2255122fa863c34c1dd7730191248344
GeorgeMitsikaris <george.mitsikaris@gmail.com>
2020-02-15 12:02:21 +0200

    Added delete confirmation functionality

--- DotNetCoreTutorial/wwwroot/js/CustomScript.js
+++ DotNetCoreTutorial/wwwroot/js/CustomScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿function ConfirmDelete(userId, isDeleted) {
+    const idToDelete = `delete_${userId}`;
+    const idToConfirmDelete = `confirmDelete_${userId}`;
+
+    if (isDeleted) {
+        $(`#${idToDelete}`).hide();
+        $(`#${idToConfirmDelete}`).show();
+    } else {
+        $(`#${idToDelete}`).show();
+        $(`#${idToConfirmDelete}`).hide();
+    }
+}
